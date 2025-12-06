8c2c67f8ada527b61d238628ebd0005ea52df4e0
Asad <asadshah1066@gmail.com>
2025-12-06 13:43:38 +0530

    learn about serialization n pydantic and tiktoken, re setup venv dictonary and move venv to root

---
+++ 02_tokenization/tiktokenization.py
@@ -0,0 +1,14 @@
+import tiktoken
+
+# Load GPT-4o tokenizer
+encoding = tiktoken.encoding_for_model("gpt-4o")
+
+text = """
+Hey buddy! This is Asad, Nice to meet you!
+"""
+
+# Encode text → list of token IDs
+tokens = encoding.encode(text)
+
+print(f"Tokens: {tokens}")
+print(f"Total token count: {len(tokens)}")
